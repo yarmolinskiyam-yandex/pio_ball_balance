0911174f071f18cd9f4977a02c034f6078ec05c6
Arseniy <yarmolinskiyam@gmail.com>
2023-11-23 18:31:39 +0300

    ultrasonic fix

--- src/student.cpp
+++ src/student.cpp
@@ -11,7 +11,10 @@ void controllerInit (Overlord &over)
     over.setSlider (SliderEnum::prog2, -10000, 10000);
 }
 
-
+/**
+ * @brief Выполнить одну итерацию системы управления
+ * @details Вызывается раз в 5мс
+ */
 void controllerTick (Overlord &over)
 {
     float setPoint = -over.getSetpoint ();
@@ -68,59 +71,3 @@ void controllerTick (Overlord &over)
     // Serial.print ('\t');
     // Serial.println ();
 }
-
-// /**
-//  * @brief Выполнить одну итерацию системы управления
-//  * @details Вызывается раз в 5мс
-//  */
-// void controllerTick_ (Overlord &over)
-// {
-//     /*!v Опрос слайдера чтобы получить инфу из внешнего мира */
-//     static float setPoint;
-//     // setPoint = over.getSlider (SliderEnum::setPoint);
-//     // /*!v Опрос кнопки и перевод */
-//     // setPoint *= !digitalRead (13) * 0.001;
-//     setPoint = -over.getSetpoint ();
-
-//     /*!v Переменные для хранения информации о мире */
-//     static float x, x_i, theta, w;
-    
-//     /*!v Опрос реального мира */
-//     x = -over.getCarX ();
-//     x_i = -over.getCarVel ();
-//     theta = over.getMotorTheta ();
-//     w = over.getMotorVel ();
-
-//     /*!v Регулятор положения вагончика */
-//     static float err4, x_i0;
-//     static float constexpr Kp4 = 1;
-//     err4 = setPoint - x;
-//     x_i0 = err4 * Kp4;
-
-//     /*!v Регулятор скорости вагончика */
-//     static float err3, theta_0;
-//     static float constexpr Kp3 = 1;
-//     err3 = x_i0 - x_i;
-//     theta_0 = err3 * Kp3;
-
-//     /*!v Регулятор положения мотора */
-//     static float err2, w_0;
-//     static float constexpr Kp2 = 1;
-//     err2 = theta_0 - theta;
-//     w_0 = err2 * Kp2;
-
-//     /*!v Регулятор скорости мотора */
-//     static float err1, u;
-//     static float constexpr Kp1 = 1;
-//     err1 = w_0 - w;
-//     u = err1 * Kp1;
-
-//     /*!v Выдача напряжения на мотор */
-//     over.setMotorU (u);
-
-//     /*!v Вывод отладочной информации */
-//     // Serial.print (setPoint*100);
-//     // Serial.print ('\t');
-//     // Serial.print (x*100);
-//     // Serial.println ();
-// }
